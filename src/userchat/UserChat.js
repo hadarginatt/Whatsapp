41849a5aa161d3d646fa84ebddb9d5f04f565e01
hadar ginattl <hginatt@gmail.com>
2022-04-06 17:37:34 +0300

    profile pictures addition

--- src/userchat/UserChat.js
+++ src/userchat/UserChat.js
@@ -1,14 +1,20 @@
 import React from 'react'
 import './UserChat.css'
+import image1 from '../image1.jpeg'
+
 
 function UserChat({name, time}){
     return(
         <div className='LeftMenuChat'>
             <div className="list-group-item list-group-item-action">
                 <div className="d-flex w-100 justify-content-between">
+                </div>
+                <div  className="profileImage">
+                <img src={image1} alt="..."></img>
+                </div>
                     <h5 className="mb-1">{name}</h5>
                     <small className="text-muted">{time}</small>
-                </div>
+             
                 {/**<p class="mb-1">Hello! Chat with me.</p>*/}
                 <small className="text-muted">Chat with me.</small>
             </div>
