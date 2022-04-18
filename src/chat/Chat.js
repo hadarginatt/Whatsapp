26a3b119c7424e5e2e739d1c98d3a7eba61b76cf
hadar ginattl <hginatt@gmail.com>
2022-04-18 12:28:53 +0300

    modal for image and video upload

--- src/chat/Chat.js
+++ src/chat/Chat.js
@@ -118,10 +118,34 @@ function showTypeArea(username, myMessages, setMessages){
 
                 </div>
                 <input type="username" className="form-control" id="newMessage" placeholder="New Message..."></input>
-                {/** attach button*/}
+                {/** attach button
                 <button className='button' onClick={() => { }}>
                     <i className="bi bi-paperclip"></i>
+                </button>*/}
+
+                <button type="button" class="bi bi-paperclip" data-bs-toggle="modal" data-bs-target="#exampleModal">
                 </button>
+              
+               
+                <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                  <div class="modal-dialog">
+                    <div class="modal-content">
+                      <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                      </div>
+                      <div class="modal-body">
+                       ...
+                      </div>
+                      <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                        <button type="button" class="btn btn-primary">Upload</button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+
                 {/** recording button*/}
 
                 <button className='button'>
