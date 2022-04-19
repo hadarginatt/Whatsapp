43859881dd4f6d8380087b89d2924db47438719c
Noa Eitan <noaeitan2@gmail.com>
2022-04-19 14:08:24 +0300

    register and login are designed

--- src/login/Login.js
+++ src/login/Login.js
@@ -11,37 +11,26 @@ function isValidLogin(setUserConnected, userName, navigate) {
     var name = document.getElementById("userName").value;
     var pass = document.getElementById("password").value;
     var alertPlaceholder = document.getElementById('liveAlertPlaceholder')
-    if (name == "" || name == null || pass == "" || pass == null) {
-        console.log("emptyyyyyyyyyyyyy");
-        
+    alertPlaceholder.innerHTML = ""
+    if (name == "" || name == null || pass == "" || pass == null) {        
         var wrapper = document.createElement('div')
         var type = 'warning'
         var message = 'Oops!! Empty fields! Please enter user name and password.'
-        wrapper.innerHTML = '<div className="alert alert-' + type + ' alert-dismissible" role="alert">' + message + '<button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button></div>'
+        wrapper.innerHTML = '<div class="alert alert-' + type + ' alert-dismissible" role="alert">' + message + '<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button></div>'
         alertPlaceholder.append(wrapper)
     } else if (databaseusers.find((value) => { return value.username === name && value.password === pass })) {
-        console.log("goooooooooooooood");
-
-
         // change state of name connected
         setUserConnected(name);
-
         // go to chat
-        // window.storage.setItem()
-        // window.location.replace("/chat");
-        // navigate('/chat')
         navigate('/chat', {state: userName})
-        // this.props.history.push('/chat');
-        // return (<switch to="/chat" />)
-        // this.router.push('/chat')
-        // window.history.pushState('Hadar', 'Hadar', '/chat');
+
 
     } else {
         console.log("baaaaaaaaaaaaaaadd");
         var wrapper = document.createElement('div')
         var type = 'warning'
         var message = 'Oops!! User name or Password invalid.'
-        wrapper.innerHTML = '<div className="alert alert-' + type + ' alert-dismissible" role="alert">' + message + '<button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button></div>'
+        wrapper.innerHTML = '<div class="alert alert-' + type + ' alert-dismissible" role="alert">' + message + '<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button></div>'
         alertPlaceholder.append(wrapper)
     }
 }
@@ -54,10 +43,10 @@ function Login({setUserConnected, userName, dataBase}) {
     const navigate = useNavigate()
 
     return (
-        <div>
+        <div id="page">
             <div className="row" id="header"><img src={logo}></img><p id="namePage">Login</p></div>
             <div id="inputsLogin" className="container-fluid">
-                <div className="row col-4 offset-4 justify-content-md-center">
+                <div className="row col-4 offset-4 ">
                     <div className="form-floating mb-3">
                         <input type="username" className="form-control" id="userName" placeholder="Isreal Israeli"></input>
                         <label htmlFor="floatingInput">User Name</label>
@@ -69,10 +58,10 @@ function Login({setUserConnected, userName, dataBase}) {
                     <div className="form-floating">
                         <p>Not registered? <a href="/register">Click here</a> to register</p>
                     </div>
+                    <div id="liveAlertPlaceholder"></div>
+                    <button onClick={() => { isValidLogin(setUserConnected, userName, navigate) }} type="button"
+                        className="btn btn-primary" id="liveAlertBtn">Login</button>
                 </div>
-
-                <div id="liveAlertPlaceholder"></div>
-                <button onClick={() => { isValidLogin(setUserConnected, userName, navigate) }} type="button" className="btn btn-primary" id="liveAlertBtn">Login</button>
             </div>
         </div>
     );
